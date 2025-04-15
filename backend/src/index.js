908c0e7490d97f83b876d810c0bd322707c27574
Ansh Gupta <guptaansh6031@gmail.com>
2025-04-15 18:11:19 +0530

    implemented end points for messages

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 
@@ -19,6 +20,7 @@ app.use(cookieParser())
 const port = process.env.PORT
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 
 
